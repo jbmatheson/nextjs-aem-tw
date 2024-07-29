e8ca6de23bbfa3fe6162f046a2f5354f6a4d170c
jimmatheson <jim@rmg.io>
2024-07-29 16:27:54 -0400

    fix

--- utils/renderPage.tsx
+++ utils/renderPage.tsx
@@ -1,23 +1,61 @@
-import { AEM } from "@/services/AEM";
 // Services
 import { JsonView } from "@/app/components/JsonView";
 import { PageBuilder } from "@/app/components/PageBuilder";
 
 export function renderPage(languageCode: string = "en") {
+  // eslint-disable-next-line react/display-name
   return ({ params }: { params: { slug?: string[] } }) => {
     const slug = params.slug == null ? "homepage" : params.slug.join("/");
-    console.log("Slug: ", slug);
 
-    return AEM.getPageData(slug, languageCode).then((response) => {
-      if (response == null) {
+    // TODO - Remove this mock data -----------------------------------------------
+    const page = {
+      pocPageList: {
+        items: [
+          {
+            _path: "/content/dam/ansys-cf/poc-pages/en/pages/homepage",
+            title: "Homepage",
+            components: [
+              {
+                __typename: "LocationModel",
+                _path:
+                  "/content/dam/ansys-cf/poc-pages/en/components/locationtest",
+                locationType: "Direct Office",
+              },
+            ],
+          },
+        ],
+      },
+    };
+
+    const items = page.pocPageList.items;
+    const components = items[0].components;
+    if (components == null) {
+      throw new Error("No components in response from AEM");
+    }
+
+    return (
+      <main
+        className="container"
+        data-aue-type={`Page - ${items[0].title ?? ""} Properties`}
+        data-aue-prop={items[0].title ?? "Page Template"}
+        data-aue-resource={`urn:aemconnection:${items[0]._path}/jcr:content/data/master`}
+      >
+        <JsonView src={page} />
+        {components.map((component, idx) => {
+          return <PageBuilder key={idx} fragment={component} />;
+        })}
+      </main>
+    );
+    //--------------------------------------------------------------------------------
+    /*return AEM.getPageData(slug, languageCode).then((response) => {
+             if (response == null) {
         throw new Error("No response from AEM");
       }
 
-      const items = response.pocPageList.items;
       if (items.length === 0) {
         throw new Error("No items in response from AEM");
-      }
-
+        }
+      const items = response.pocPageList.items;
       const components = items[0].components;
       if (components == null) {
         throw new Error("No components in response from AEM");
@@ -37,5 +75,6 @@ export function renderPage(languageCode: string = "en") {
         </main>
       );
     });
+  };*/
   };
 }
